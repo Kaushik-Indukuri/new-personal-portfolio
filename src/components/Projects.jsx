d399e5e61ad72c85b89e94a66351b396a112ce1a
Kaushik-Indukuri <49111141+Kaushik-Indukuri@users.noreply.github.com>
2024-07-31 03:20:15 -0700

    changed assets for deployment

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,49 +8,49 @@ const Projects = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-16 md:gap-12 lg:gap-20">
                 <ProjectCard
                     projectId="youtube-comments-dashboard"
-                    imgSrc="./src/assets/yt_art.png"
+                    imgSrc="/assets/yt_art.png"
                     projectTitle="Youtube Comments Dashboard"
                     technologies="React, TypeScript, Tailwind, MongoDB, Kafka, Python, FastAPI, Docker, K8s, Terraform, Github Actions"
                 />
                 <ProjectCard
                     projectId="http-client-server"
-                    imgSrc="./src/assets/project4.png"
+                    imgSrc="/assets/http.png"
                     projectTitle="HTTP Client & Server"
                     technologies="C, Socket Programming, Linux"
                 />
                 <ProjectCard
                     projectId="lenet-acceleration"
-                    imgSrc="./src/assets/project6.png"
+                    imgSrc="/assets/lenet.png"
                     projectTitle="LeNet-5 Neural Network GPU Acceleration"
                     technologies="C, CUDA, Nsight-Compute"
                 />
                 <ProjectCard
                     projectId="linux-operating-system"
-                    imgSrc="./src/assets/project3.png"
+                    imgSrc="/assets/linux_os.png"
                     projectTitle="Linux Operating System"
                     technologies="C, x86 Assembly, Linux"
                 />
                 <ProjectCard
                     projectId="stock-sentiment-api"
-                    imgSrc="./src/assets/project1.png"
+                    imgSrc="/assets/stock_api.png"
                     projectTitle="Stock Sentiment API"
                     technologies="Python, FastAPI, Pandas, Numpy, BeautifulSoup, Docker, AWS Lambda"
                 />
                 <ProjectCard
                     projectId="airport-route-finder"
-                    imgSrc="./src/assets/project5.png"
+                    imgSrc="/assets/route_finder.png"
                     projectTitle="Airport Route Finder"
                     technologies="C++, CMake, Docker"
                 />
                 <ProjectCard
                     projectId="inspeech"
-                    imgSrc="./src/assets/inspeech.png"
+                    imgSrc="/assets/inspeech.png"
                     projectTitle="InSpeech Mobile App"
                     technologies="Flutter, Firebase, Speech-to-Text, Web Speech API, OCR"
                 />
                 <ProjectCard
                     projectId="fanua"
-                    imgSrc="./src/assets/Fanua.png"
+                    imgSrc="/assets/fanua.png"
                     projectTitle="Fanua Mobile App"
                     technologies="Flutter, Flask, Firebase, Realtor API, Docker, AWS EC2"
                 />
