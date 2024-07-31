a794640e493f3f6a73dc686e583fc878a1e9ed4b
Kaushik-Indukuri <49111141+Kaushik-Indukuri@users.noreply.github.com>
2024-07-31 03:47:41 -0700

    updated server script

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-const history = require('connect-history-api-fallback');
-const http = require('http');
-const path = require('path');
-const express = require('express');
+import history from 'connect-history-api-fallback';
+import http from 'http';
+import path from 'path';
+import express from 'express';
+import { fileURLToPath } from 'url';
+
 const app = express();
 
-// Middleware for serving 'index.html' for all routes
+// Use the connect-history-api-fallback middleware
 app.use(history());
+
+// Serve static files from the dist directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// Create an HTTP server
 const server = http.createServer(app);
+
+// Start the server
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
